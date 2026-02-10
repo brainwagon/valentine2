983215ef9a452b05f072abff18b5637e1e31216f
Mark VandeWettering <mvandewettering@gmail.com>
2026-02-10 09:06:56 -0800

    conductor(checkpoint): Checkpoint end of Phase 1: Project Foundation & Environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^three$': '<rootDir>/tests/mocks/three.js',
+    '^three/addons/controls/OrbitControls.js$': '<rootDir>/tests/mocks/three-addons.js',
+    '^@dimforge/rapier3d-compat$': '<rootDir>/tests/mocks/rapier.js'
+  }
 };
